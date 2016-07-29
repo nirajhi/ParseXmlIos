99f94e18b32a54faa101b6ab0a6552d75cae4536
Niraj Hirachan <nirajhirachan147@gmail.com>
2016-07-29 11:21:48 +0545

    xmldocument parsing using TouchXML  feature/arc type

--- ParseXML/ViewController.h
+++ ParseXML/ViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "TouchXML.h"
 
 @interface ViewController : UIViewController
 
--- ParseXML/ViewController.m
+++ ParseXML/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+NSString *appDataPath;
 @interface ViewController ()
 
 @end
@@ -16,6 +16,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSString *stringXML;
+    stringXML = @"<pigletlist><piglet><name>hari</name><age>12</age></piglet></pigletlist>";
+    
+    //stringXML = @"<pigletlist><piglet id=\"1\"><name>Nifnif</name></piglet><piglet id=\"2\"><name>Nufnuf</name></piglet><piglet id=\"3\"><name>Nafnaf</name></piglet></pigletlist>";
+    //stringXML = @"<pigletlist><piglet id=\"1\"><name>Nifnif</name><name1>Nifnif</name1></piglet><piglet id=\"2\"><name>Nufnuf</name><name1>Nifnif</name1></piglet><piglet id=\"3\"><name>Nafnaf</name><name1>Nifnif</name1></piglet></pigletlist>";
+    //stringXML = @"<pigletlist><piglet id=\"1\"><name>Nifnif</name></piglet></pigletlist><pigletlista><pigleta id=\"1\"><namea>Nifnif</namea></pigleta></pigletlista>";
+    NSArray *array = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    appDataPath = [NSString stringWithFormat:@"%@", [array objectAtIndex:0]];
+    
+    NSString *fileName = [NSString stringWithFormat:@"%@/example.xml",
+                          appDataPath];
+    
+    [stringXML writeToFile:fileName
+                atomically:NO
+                  encoding:NSStringEncodingConversionAllowLossy
+                     error:nil];
+
+    NSMutableArray *res = [[NSMutableArray alloc] init];
+    
+    NSData *dataFromXMLFile = [NSData dataWithContentsOfFile:fileName];
+    
+    CXMLDocument *doc = [[CXMLDocument alloc] initWithData:dataFromXMLFile options:0 error:nil];
+    NSArray *nodes = NULL;
+    
+    nodes = [doc nodesForXPath:@"//pigletlist/piglet/name" error:nil];
+    
+   //int counter;
+    NSString *ns;
+    for(CXMLElement *node in nodes){
+        ns = [node stringValue];
+    }
+    NSLog(@"%@",ns);
+    
+//    for (CXMLElement *node in nodes) {
+//        // nodenames = [element name];
+//        
+//        NSLog(@"Child nodes:%lu",[node childCount]);
+//        NSMutableDictionary *item = [[NSMutableDictionary alloc] init];
+//        int counter;
+//        
+//        for(counter = 0; counter < [node childCount]; counter++) {
+//            //  common procedure: dictionary with keys/values from XML node
+//            [item setObject:[[node childAtIndex:counter] stringValue] forKey:[[node childAtIndex:counter] name]];
+//        }
+//        
+//        //  and here it is - attributeForName! Simple as that.
+//        [item setObject:[[node attributeForName:@"id"] stringValue] forKey:@"id"];  // <------ this magical arrow is pointing to the area of interest
+//        
+//        [res addObject:item];
+//    }
+//    //
+  //  NSLog(@"%@", res);
+    
+  //  NSLog(@"%@",appDataPath);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
